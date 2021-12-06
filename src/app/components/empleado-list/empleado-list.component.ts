eda07f9a5f0e82b56f27b00ef861dc48d147ad5a
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-06 20:22:09 -0300

    implemented modularization with input and output

--- src/app/components/empleado-list/empleado-list.component.ts
+++ src/app/components/empleado-list/empleado-list.component.ts
@@ -1,3 +1,4 @@
+import { Empleado } from './../../models/Empleado';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpleadoListComponent implements OnInit {
 
+  listEmpleados: Empleado[]= [
+    {legajo:1, nombre:"Juan", apellido:"Perez", sexo:"Masculino", salario: 25000},
+    {legajo:2, nombre:"Carlos", apellido:"Gomez", sexo:"Masculino", salario: 35000},
+    {legajo:3, nombre:"Sandra", apellido:"Juarez", sexo:"Femenino", salario: 40000},
+    {legajo:4, nombre:"Diego", apellido:"Luna", sexo:"Masculino", salario: 41000},
+    {legajo:5, nombre:"Monica", apellido:"Lewinsky", sexo:"Femenino", salario: 38000},
+    {legajo:6, nombre:"Pepe", apellido:"Argento", sexo:"Masculino", salario: 60000}
+  ];
+
+  radioButtonSeleccionado = "Todos";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  obtenerTotalEmpleados(): number {
+    return this.listEmpleados.length;
+  }
+
+  obtenerTotalFemeninos(): number {
+    return this.listEmpleados.filter(list => list.sexo === "Femenino").length;
+  }
+
+  obtenerTotalMasculinos(): number {
+    return this.listEmpleados.filter(list => list.sexo === "Masculino").length;
+  }
+
+  empleadoCountRadioButtonChange(radioButtonSelect:string):void{
+    this.radioButtonSeleccionado = radioButtonSelect;
   }
 
 }
