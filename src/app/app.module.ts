1bc2820296e6b249f5e159ab8d3e70ac39167681
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-06 18:40:27 -0300

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpleadoListComponent } from './components/empleado-list/empleado-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoListComponent
   ],
   imports: [
     BrowserModule,
